fc65e91a5dd8c892f85eaadf1b128d1d93aec804
ynportfolios <y.n.portfolios@gmail.com>
2021-02-18 13:42:49 +0900

    タスクの新規作成画面を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,9 +8,32 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'タスクを登録しました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスク登録に失敗しました。'
+      render new_task_path
+    end
+  end
+
   private
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :content
+    )
   end
 end
