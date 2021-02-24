1cd3b12233269db638dc4d9fa27fc0c3b4988f4b
ynportfolios <y.n.portfolios@gmail.com>
2021-02-24 11:30:34 +0900

    システムスペックの追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -75,4 +75,44 @@ RSpec.describe 'タスク管理機能', type: :system do
        end
      end
   end
+  describe '検索機能' do
+    before do
+      # 必要に応じて、テストデータの内容を変更して構わない
+      FactoryBot.create(:task, name: "task", status: 1)
+      FactoryBot.create(:task, name: "sample", status: 2)
+    end
+    context 'タイトルであいまい検索をした場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        visit root_path
+        # タスクの検索欄に検索ワードを入力する (例: task)
+        fill_in 'task_name_field', with: 'task'
+        # 検索ボタンを押す
+        click_on '検索'
+        expect(page).to have_content 'task'
+        expect(page).to_not have_content 'sample'
+      end
+    end
+    context 'ステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        # ここに実装する
+        # プルダウンを選択する「select」について調べてみること
+        visit root_path
+        select '未着手', from: 'status_field'
+        click_on '検索'
+        expect(page).to have_content 'task'
+        expect(page).to_not have_content 'sample'
+      end
+    end
+    context 'タイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        # ここに実装する
+        visit root_path
+        fill_in 'task_name_field', with: 'sample'
+        select '着手', from: 'status_field'
+        click_on '検索'
+        expect(page).to have_content 'sample'
+        expect(page).to_not have_content 'task'
+      end
+    end
+  end
 end
