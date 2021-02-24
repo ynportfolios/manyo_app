2704298f00f3b0692cec3f3993bfef8c1ebe34a5
ynportfolios <y.n.portfolios@gmail.com>
2021-02-24 10:59:47 +0900

    検索機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,12 @@ class TasksController < ApplicationController
   def index
     if params[:sort_expired]
       @tasks = Task.all.order("deadline DESC")
+    elsif params[:name] && params[:status] && !(params[:status] == 'なし') && !(params[:name] == '')
+      @tasks = Task.search_name_status(params[:name], params[:status])
+    elsif params[:name] && (params[:status] == 'なし') && !(params[:name] == '')
+      @tasks = Task.search_name(params[:name])
+    elsif params[:status] && (params[:status] != 'なし') && (params[:name] == '')
+      @tasks = Task.search_status(params[:status])
     else
       @tasks = Task.all.order("created_at DESC")
     end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,21 @@
 class Task < ApplicationRecord
 
-  enum status: { 未着手: 0, 着手: 1, 完了: 2 }
+  enum status: { なし: 0, 未着手: 1, 着手: 2, 完了: 3 }
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { minimum: 1, maximum: 200 }
   validates :deadline, presence: true
   validates :status, presence: true
+
+  scope :search_name_status, ->(name, status) do
+    where('name LIKE ?', "%#{name}%").where(status: status)
+  end
+
+  scope :search_name, ->(name) do
+    where('name LIKE ?', "%#{name}%")
+  end
+
+  scope :search_status, ->(status) do
+    where(status: status)
+  end
 end
