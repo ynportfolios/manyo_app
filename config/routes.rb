5f59a682087b48b68ca6eb317b04dda33ec627f2
ynportfolios <y.n.portfolios@gmail.com>
2021-02-25 13:45:27 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
