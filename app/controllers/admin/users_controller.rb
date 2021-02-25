752325bfd77601b63eefc712f64d6949199bf71c
ynportfolios <y.n.portfolios@gmail.com>
2021-02-25 16:35:06 +0900

    管理画面の実装

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,44 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  def index
+    @users = User.all.includes(:tasks)
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path(@user.id)
+    else
+      render :new
+    end
+  end
+  def edit
+  end
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'ユーザを編集しました。'
+      redirect_to admin_user_path(@user.id)
+    else
+      flash.now[:danger] = 'タスクの編集に失敗しました。'
+      render :edit
+    end
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+  def destroy
+    @user.destroy
+    flash[:success] = 'タスクを削除しました。'
+    redirect_to admin_users_path
+  end
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :tasks
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  namespace :admin do
+    resources :users
+  end
 end
