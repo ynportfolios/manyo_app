28083d326bc7799297eb43841f7bde571ce82ed9
ynportfolios <y.n.portfolios@gmail.com>
2021-02-25 11:19:40 +0900

    ユーザモデルの作成

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'kaminari', '~> 0.17.0'
 
 gem 'kaminari-bootstrap', '~> 3.0.1'
 
+gem 'bcrypt', '3.1.11'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  before_validation { email.downcase! }
+
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, 
+                    uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
