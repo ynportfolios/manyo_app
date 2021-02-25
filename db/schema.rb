b3d075ba43a657e774b417fde9e0ca63f045ab8d
ynportfolios <y.n.portfolios@gmail.com>
2021-02-25 11:38:17 +0900

    ユーザとタスクを紐づけ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_015701) do
+ActiveRecord::Schema.define(version: 2021_02_25_023503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_02_25_015701) do
     t.datetime "deadline", default: "2021-03-01 00:00:00", null: false
     t.integer "status", default: 0, null: false
     t.integer "priority", default: 0, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_02_25_015701) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
