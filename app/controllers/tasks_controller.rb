3c4f1ae7a5d8d8a247e54f2634ee9ff58c742ad6
ynportfolios <y.n.portfolios@gmail.com>
2021-03-02 15:39:20 +0900

    ラベルによる検索機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,9 @@ class TasksController < ApplicationController
   PER = 3
 
   def index
-    if params[:sort_expired]
+    if params[:label_id]
+      @tasks = @current_user.tasks.joins(:labels).where(labels: { id: params[:label_id] }).page(params[:page]).per(PER)
+    elsif params[:sort_expired]
       @tasks = @current_user.tasks.order("deadline DESC").page(params[:page]).per(PER)
     elsif params[:sort_priority]
       @tasks = @current_user.tasks.order("priority DESC").page(params[:page]).per(PER)
